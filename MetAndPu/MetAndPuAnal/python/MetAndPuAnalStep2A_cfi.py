2fd57b7c8f3099925fe4279309ebb13483f38839
Alexis Kalogeropoulos <alkaloge@cern.ch>
2020-06-28 09:43:17 -0500

    commit

--- MetAndPu/MetAndPuAnal/python/MetAndPuAnalStep2A_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-metandpuanalstep2a = cms.EDAnalyzer('MetAndPuAnalStep2A',
-                                    doprints = cms.bool(True),      
-                                    mets     = cms.InputTag("slimmedMETs")
-                                    )
--- MetAndPu/MetAndPuAnal/python/MetAndPuAnalStep3_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-metandpuanalstep3 = cms.EDAnalyzer('MetAndPuAnalStep3',
-                                   vertices  = cms.InputTag("offlineSlimmedPrimaryVertices"),
-                                   mets       = cms.InputTag("slimmedMETs"),
-                                   metspuppi  = cms.InputTag("slimmedMETsPuppi"),
-                                   muons     = cms.InputTag("slimmedMuons"),
-                                   )
